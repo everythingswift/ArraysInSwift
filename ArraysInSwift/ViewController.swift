8fe92147ad5fadd34165b8b40acc95ac24f928dc
Sridhar Sanapala <sridhar@everythingswift.com>
2015-03-08 14:34:33 -0500

    Added files for ArraysInSwift
    Added code for Introduction to Arrays in Swift.

---
+++ ArraysInSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ArraysInSwift
+//
+//  Created by Sridhar Sanapala on 2/24/15.
+//  Copyright (c) 2015 Sridhar Sanapala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# ArraysInSwift
+To show different ways of array creation and manipulation in Swift Programming language.
